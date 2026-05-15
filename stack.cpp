89aee0e20b719d483b32fad511a47e2d66745c7c
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-15 14:45:01 +0700

    Node temp

--- stack.cpp
+++ stack.cpp
@@ -39,6 +39,15 @@ int push(int value)
 
 void pop()
 {
-    Node *temp = top
+    Node *temp = top;
+    if (isEmpty())
+    {
+        cout << "Stack is empty." << endl;
+        return;
+    }
+
+    cout << "Popped value: " << temp->data << endl;
+    top = temp->next;
+    delete temp;
 }
-}
+};

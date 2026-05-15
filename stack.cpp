eb1ec2916b7ddfb6630f964b199052656c446dab
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-15 14:33:28 +0700

    void

--- stack.cpp
+++ stack.cpp
@@ -30,5 +30,12 @@ int push(int value)
 {
     Node *newNode = new Node();
     newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    cout << "Push value: " << value << endl;
+    return value;
 }
+
+
+void 
 }
